b50fe18b394f5287e4467b46a5820d575d5b6bfc
cloudcome <cloudcome@cloudcome.com>
2024-02-24 09:36:55 +0800

    add  verify testing

---
+++ verifier/ctx_skb_check_leak_addr/test_verifier.c
@@ -0,0 +1,87 @@
+#include <unistd.h>
+#include <bpf.h>
+#include "bpf_insn.h"
+#include <error.h>
+#include <errno.h>
+#include <stdio.h>
+
+#ifndef __NR_bpf
+# if defined(__i386__)
+#  define __NR_bpf 357
+# elif defined(__x86_64__)
+#  define __NR_bpf 321
+# elif defined(__aarch64__)
+#  define __NR_bpf 280
+# elif defined(__sparc__)
+#  define __NR_bpf 349
+# elif defined(__s390__)
+#  define __NR_bpf 351
+# elif defined(__mips__) && defined(_ABIO32)
+#  define __NR_bpf 4355
+# elif defined(__mips__) && defined(_ABIN32)
+#  define __NR_bpf 6319
+# elif defined(__mips__) && defined(_ABI64)
+#  define __NR_bpf 5315
+# else
+#  error __NR_bpf not defined. libbpf does not support your arch.
+# endif
+#endif
+
+
+static int map_fd;
+
+static void maps_create(void)
+{   
+    union bpf_attr attr;
+    memset(&attr, 0, sizeof(attr));
+    //attr.map_name = 'cloud';
+	attr.map_type = BPF_MAP_TYPE_ARRAY;
+	attr.key_size = 4;
+	attr.value_size = 4;
+	attr.max_entries = 1;
+	attr.map_flags = 0;
+    map_fd = syscall(__NR_bpf, BPF_MAP_CREATE, &attr, sizeof(attr));  
+
+	if (map_fd < 0) {
+		printf("failed to create map '%s'\n", strerror(errno));
+	}
+}
+
+static int test_verifier(void)
+{
+    static char bpf_log_buf[65536];
+    static const char bpf_license[] = "GPL";
+    int bpf_fd;
+    
+    //R2 leaks addr into ctx(root is ok)
+    const struct bpf_insn prog[] = {
+	    BPF_MOV64_IMM(BPF_REG_0, 0),
+	    BPF_LD_MAP_FD(BPF_REG_2, map_fd
+        ),
+	    BPF_STX_MEM(BPF_DW, BPF_REG_1, BPF_REG_2,
+		      offsetof(struct __sk_buff, cb[0])),
+	    BPF_EXIT_INSN(),
+	   };
+    union bpf_attr attr;
+
+    memset(&attr, 0, sizeof(attr));
+    attr.prog_type = BPF_PROG_TYPE_SOCKET_FILTER;
+    attr.insn_cnt = (__u32)(sizeof(prog) / sizeof(struct bpf_insn));
+    attr.insns = (unsigned long) &prog;
+    attr.license = (unsigned long) &bpf_license;
+    attr.log_buf = (unsigned long) &bpf_log_buf;
+    attr.log_size = sizeof(bpf_log_buf);
+    attr.log_level = 2;
+    attr.kern_version = 0;
+
+    bpf_fd = syscall(__NR_bpf, BPF_PROG_LOAD, &attr, sizeof(attr));
+	if (bpf_fd < 0)
+		error(1, errno, "ebpf error. log:\n%s\n", bpf_log_buf);
+    return bpf_fd;
+}
+
+int main(void)
+{   
+    maps_create();  
+	return test_verifier();
+}
---
+++ verifier/ctx_sklook_check_read/test_verifier.c
@@ -0,0 +1,69 @@
+#include <unistd.h>
+//#include <bpf.h>
+#include "bpf_insn.h"
+#include <error.h>
+#include <errno.h>
+#include <linux/bpf.h>
+#include <stddef.h>
+#include <string.h>
+
+#ifndef __NR_bpf
+# if defined(__i386__)
+#  define __NR_bpf 357
+# elif defined(__x86_64__)
+#  define __NR_bpf 321
+# elif defined(__aarch64__)
+#  define __NR_bpf 280
+# elif defined(__sparc__)
+#  define __NR_bpf 349
+# elif defined(__s390__)
+#  define __NR_bpf 351
+# elif defined(__mips__) && defined(_ABIO32)
+#  define __NR_bpf 4355
+# elif defined(__mips__) && defined(_ABIN32)
+#  define __NR_bpf 6319
+# elif defined(__mips__) && defined(_ABI64)
+#  define __NR_bpf 5315
+# else
+#  error __NR_bpf not defined. libbpf does not support your arch.
+# endif
+#endif
+
+
+static int test_verifier(void)
+{
+    static char bpf_log_buf[65536];
+    static const char bpf_license[] = "GPL";
+    int bpf_fd;
+    
+    //invalid 8-byte reads from a 4-byte fields in bpf_sk_lookup
+    /* Memory load, dst_reg = *(uint *) (src_reg + off16) */
+    /* BPF_LDX_MEM(SIZE, DST, SRC, OFF)	*/
+    const struct bpf_insn prog[] = {
+		BPF_LDX_MEM(BPF_W, BPF_REG_0, BPF_REG_1,
+			    offsetof(struct bpf_sk_lookup, family)),
+		BPF_MOV32_IMM(BPF_REG_0, 0),
+        BPF_EXIT_INSN(),
+	   };
+    union bpf_attr attr;
+
+    memset(&attr, 0, sizeof(attr));
+    attr.prog_type = BPF_PROG_TYPE_SK_REUSEPORT;
+    attr.insn_cnt = (__u32)(sizeof(prog) / sizeof(struct bpf_insn));
+    attr.insns = (unsigned long) &prog;
+    attr.license = (unsigned long) &bpf_license;
+    attr.log_buf = (unsigned long) &bpf_log_buf;
+    attr.log_size = sizeof(bpf_log_buf);
+    attr.log_level = 2;
+    attr.kern_version = 0;
+
+    bpf_fd = syscall(__NR_bpf, BPF_PROG_LOAD, &attr, sizeof(attr));
+	if (bpf_fd < 0)
+		error(1, errno, "ebpf error. log:\n%s\n", bpf_log_buf);
+    return bpf_fd;
+}
+
+int main(void)
+{
+	return test_verifier();
+}
